fd126cd580dce31eeb1e68c657c0294485902f25
toSHA-256 <antonsmorgun@gmail.com>
2023-06-12 18:02:37 +0300

    models_work

--- it_school/mainpage/models.py
+++ it_school/mainpage/models.py
@@ -4,10 +4,12 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     available_courses = models.TextField()
-    wallet = 0
+    wallet = models.IntegerField(default=0)
     is_student = models.BooleanField(default=True)
     is_mentor = models.BooleanField(default=False)
     courses = models.ManyToManyField('Course', blank=True)
+    groups = models.ManyToManyField('auth.Group', blank=True, related_name='customuser_set')
+    user_permissions = models.ManyToManyField('auth.Permission', blank=True, related_name='customuser_set')
 
     def __str__(self):
         return self.username
@@ -23,7 +25,7 @@ class Course(models.Model):
     difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
     rating = models.DecimalField(max_digits=3, decimal_places=1)
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    mentor = models.ForeignKey('Mentor', on_delete=models.CASCADE)
+    mentor = models.ForeignKey('CustomUser', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
