e0f5a44caee41b38982c46327bbc82bcca6059f1
toSHA-256 <antonsmorgun@gmail.com>
2023-06-12 17:49:32 +0300

    add_model_course

--- it_school/mainpage/models.py
+++ it_school/mainpage/models.py
@@ -7,6 +7,23 @@ class CustomUser(AbstractUser):
     wallet = 0
     is_student = models.BooleanField(default=True)
     is_mentor = models.BooleanField(default=False)
+    courses = models.ManyToManyField('Course', blank=True)
 
     def __str__(self):
         return self.username
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    DIFFICULTY_CHOICES = [
+        ('beginner', 'Начинающий'),
+        ('advanced', 'Продвинутый'),
+    ]
+    difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    mentor = models.ForeignKey('Mentor', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
