d9fe67da403a75855544e558f1cc7a9e8d16a12c
toSHA-256 <antonsmorgun@gmail.com>
2023-06-20 12:42:21 +0300

    add_group

--- it_school/registration/models.py
+++ it_school/registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 from phonenumber_field.modelfields import PhoneNumberField
 
 
@@ -7,7 +9,8 @@ class CustomUser(AbstractUser):
     wallet = models.DecimalField(max_digits=6, decimal_places=2, default=0)  # кошелёк
     is_student = models.BooleanField(default=True)  # чекбокс является ли студентом
     is_mentor = models.BooleanField(default=not is_student)  # чекбокс является ли ментором
-    courses = models.ManyToManyField('mainpage.Course', blank=True)  # список курсов к которому у порльзователя имеется доступ
+    courses = models.ManyToManyField('mainpage.Course', blank=True,
+                                     default=[])  # список курсов к которому у порльзователя имеется доступ
     groups = models.ManyToManyField('auth.Group', blank=True, related_name='customuser_set')
     user_permissions = models.ManyToManyField('auth.Permission', blank=True, related_name='customuser_set')
     phone_number = PhoneNumberField(null=False, blank=False, unique=True)  # телефонный номер с проверкой
