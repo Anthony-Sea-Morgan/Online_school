0310f73667aba98519931533c8c45e946f0b4d80
fgost <fgost31@gmail.com>
2023-06-21 23:00:11 +0300

    Revert "Vadim v0.0.4.1:"
    This reverts commit 58f0ea853d51c669e48e3221526db5ca4b8b4744.

--- it_school/mainpage/static/dist/js/courseDescAct.js
+++ it_school/mainpage/static/dist/js/courseDescAct.js
@@ -1,8 +1,2 @@
 let descriptionFull = document.getElementsByClassName('description-content')[0];
 descriptionFull.innerHTML = descriptionFull.innerText;
-function scrollToAnchor(anchorId) {
-  const anchor = document.getElementById(anchorId);
-  if (anchor) {
-    anchor.scrollIntoView({ behavior: 'smooth' });
-  }
-}
