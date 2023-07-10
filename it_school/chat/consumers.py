1f1adbcebce87762e77eeab39b68351023e1570f
toSHA-256 <antonsmorgun@gmail.com>
2023-07-10 16:15:18 +0300

    чат авторизован

--- it_school/chat/consumers.py
+++ it_school/chat/consumers.py
@@ -1,45 +1,54 @@
 import json
-from asgiref.sync import async_to_sync
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
+from datetime import datetime
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Присоединение к группе комнаты
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name,
             self.channel_name
         )
 
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # Покидание группы комнаты
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         # Получение сообщения от клиента
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        author = self.scope['user'].username
+        timestamp = datetime.now().strftime('%H:%M')  # Получение текущего времени
 
         # Отправка сообщения всем в группе комнаты
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message
+                'message': message,
+                'author': author,
+                'timestamp': timestamp
             }
         )
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         # Отправка сообщения клиенту
         message = event['message']
+        author = event['author']
+        timestamp = event['timestamp']
 
-        self.send(text_data=json.dumps({
-            'message': message
+        await self.send(text_data=json.dumps({
+            'message': message,
+            'author': author,
+            'timestamp': timestamp
         }))
