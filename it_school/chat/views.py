b4d7553f14c89341705b8d4a4a7d765f29335551
fgost <fgost31@gmail.com>
2023-07-12 17:41:59 +0300

    Vadim v0.2.22:

--- it_school/chat/views.py
+++ it_school/chat/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from mainpage.models import Lesson
+from mainpage.models import Lesson, CustomGroup
+from management.forms import LessonForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -13,8 +14,11 @@ def chat_room(request, room_name):
     """
     user = request.user
     lesson = Lesson.objects.filter(pk=room_name).first()
+    group = CustomGroup.objects.get(course_owner_id=lesson.course_owner_id)
     return render(request, 'chat/lobby.html', {
         'room_name': room_name,
         'lesson': lesson,
-        'user': user
+        'user': user,
+        'lesson_form': LessonForm(),
+        'group': group,
     })
