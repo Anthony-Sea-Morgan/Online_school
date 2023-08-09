3e75e33dfa8f299466a41907f60a34c0bb5096c1
fgost <fgost31@gmail.com>
2023-08-09 05:10:48 +0300

    Vadim fixes:

--- it_school/registration/forms.py
+++ it_school/registration/forms.py
@@ -16,10 +16,10 @@ class RegisterUserForm(UserCreationForm):
     first_name = forms.CharField(label='Имя:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
     last_name = forms.CharField(label='Фамилия:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
     email = forms.EmailField(label='Почтовый адрес:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
-    phone = PhoneNumberField(label='Номер телефона:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
+    phone_number = PhoneNumberField(label='Номер телефона:', widget=forms.TextInput(attrs={'class': 'blank-input'}))
     password1 = forms.CharField(label='Пароль:', widget=forms.PasswordInput(attrs={'class': 'blank-input'}))
     password2 = forms.CharField(label='Подтверждение пароля:', widget=forms.PasswordInput(attrs={'class': 'blank-input'}))
 
     class Meta:
         model = CustomUser
-        fields = ('first_name','last_name', 'email', 'phone', 'password1', 'password2')
+        fields = ('first_name','last_name', 'email', 'phone_number', 'password1', 'password2')
